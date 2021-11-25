019ff7b79dc1081273c4ebc27503606596fe1682
joy <utsho233@gmail.com>
2021-11-25 22:41:42 +0600

    adding img sadia vijoy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Bijoy from './Components/Blog_1/Bijoy'
 
 function App() {
   return (
     <div className="App">
       <h1>I love you Sadia.</h1>
+      <Bijoy />
     </div>
   );
 }
---
+++ src/Components/Blog_1/Bijoy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sadia1 from '../IMG/Sadia_bijoy.png'
+
+
+const Bijoy = () => {
+    return (
+        <div>
+            <img src = {Sadia1}  alt="" />
+        </div>
+    )
+}
+
+export default Bijoy
